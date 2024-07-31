53b60118afd16ce836617033e995cdd80bb9801c
amkhrjee <aniruddha.mukherji@outlook.com>
2024-07-31 15:07:18 +0530

    fix: css index

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   daisyui: {
-    themes: ["lemonade", "sunset"],
+    themes: ["lemonade", "dark"],
   },
   content: ["./*.html"],
   theme: {
